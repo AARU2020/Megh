ba515971b22c955ce22e42d875b30d93de47659e
satish <satish.insurejoy@gmail.com>
2022-08-02 17:39:09 +0530

    initial commit

---
+++ src/main/java/com/insurejoy/hello/my_first_system/App.java
@@ -0,0 +1,13 @@
+package com.insurejoy.hello.my_first_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
